ba2ea3336d50f4add1e5c142dcf69fbdb3d02ef5
Gabriel <102471717+MapPoints@users.noreply.github.com>
2026-07-20 15:28:44 +0800

    Add files via upload

--- includes/navbar.php
+++ includes/navbar.php
@@ -6,13 +6,13 @@
 
     <ul>
 
-        <li><a href="index.php">Home</a></li>
+        <li><a href="index.php" class="nav-link">Home</a></li>
 
-        <li><a href="#">Products</a></li>
+        <li><a href="#" class="nav-link">Products</a></li>
 
-        <li><a href="login.php">Login</a></li>
+        <li><a href="login.php" class="btn-nav-outline">Login</a></li>
 
-        <li><a href="register.php">Register</a></li>
+        <li><a href="register.php" class="btn-nav-primary">Register</a></li>
 
     </ul>
 
